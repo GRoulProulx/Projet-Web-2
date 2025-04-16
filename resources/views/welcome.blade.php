51ea0895d3011f01f5c33998ab3918fca5009edb
sylsylviane <syl.sylviane1@gmail.com>
2025-04-16 12:31:40 -0400

    Créer une démo pour présenter les variables de style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html>
+<html lang="fr">
 
 <head>
     <meta charset="utf-8" />
@@ -7,10 +7,37 @@
     @vite('resources/css/app.css')
 </head>
 
-<body>
-    <h1 class="text-3xl text-amber-300 font-bold underline">
-        Hello world!
-    </h1>
+<body class="bg-white font-family font-light m-md">
+    <section>
+        <h1 class="font-family-title text-lg text-blue-magenta font-bold">
+            Hello world!
+        </h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos nemo quidem obcaecati optio provident a architecto omnis! Nesciunt, excepturi? Assumenda quia similique ratione quasi optio consequatur doloribus libero hic expedita!</p>
+    </section>
+
+    <section class="mt-md">
+        <header class="mb-xs">
+            <h1 class="font-family-title text-lg text-taupe font-bold">
+                Hello world!
+            </h1>
+        </header>
+        <div class="flex flex-col gap-xxs">
+            <h2 class="font-regular uppercase">Un sous-titre</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos nemo quidem obcaecati optio provident a architecto omnis! Nesciunt, excepturi? Assumenda quia similique ratione quasi optio consequatur doloribus libero hic expedita!</p>
+        </div>
+    </section>
+
+    <button class="bg-blue-magenta text-white font-regular text-sm py-xxs px-md rounded-md mt-md">
+        <span>Cliquez ici</span>
+    </button>
+
+    <button class="bg-taupe text-white font-regular text-sm py-xs px-md rounded-md mt-md">
+        <span>Cliquez ici</span>
+    </button>
+
+    <button class="bg-gold text-white font-regular text-sm py-xs px-md rounded-md mt-md">
+        <span>Cliquez ici</span>
+    </button>
 </body>
 
 </html>
