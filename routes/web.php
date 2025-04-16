436aaa33437a847703e4f114e43af5e0728a32bc
GRoulProulx <gproulx85@gmail.com>
2025-04-16 15:42:09 -0400

    Création des routes pour les usagers et l'authentification

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ScraperController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +19,20 @@ use App\Http\Controllers\ScraperController;
 Route::get('/', function () {
     return view('welcome');
 });
+// Routes UTILISATEURS
+// TODO: Le reste des routes pour l'oublie du mot de passe, la modification du mot de passe, etc.
 
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+Route::post('/users', [UserController::class, 'store'])->name('users.store');
+
+
+// Routes AUTHENTIFICATION
+Route::get('/login', [AuthController::class, 'create'])->name('login');
+Route::post('/login', [AuthController::class, 'store'])->name('login.store');
+Route::get('/logout', [AuthController::class, 'destroy'])->name('logout');
+
+
+// Route pour le scraper
 Route::get('/test-scraper', [ScraperController::class, 'test']);
 
