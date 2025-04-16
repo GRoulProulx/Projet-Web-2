5c8a7f31dbb7e81d471c734cbbea875f780071e9
sylsylviane <syl.sylviane1@gmail.com>
2025-04-16 15:37:36 -0400

    Ajouter les routes pour gérer les bouteilles

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ScraperController;
-
+use App\Http\Controllers\BottleController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,5 +18,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// ROUTES POUR GÉRER LES BOUTEILLES
+Route::get('/bottles', [BottleController::class, 'index'])->name('bottle.index');
+Route::get('/bottle/{bottle}', [BottleController::class, 'show'])->name('bottle.show');
+Route::get('/create/bottle', [BottleController::class, 'create'])->name('bottle.create');
+Route::post('/create/bottle', [BottleController::class, 'store'])->name('bottle.store');
+Route::get('/edit/bottle/{bottle}', [BottleController::class, 'edit'])->name('bottle.edit');
+Route::put('/edit/bottle/{bottle}', [BottleController::class, 'update'])->name('bottle.update');
+Route::delete('/bottle/{bottle}', [BottleController::class, 'destroy'])->name('bottle.destroy');
+
+// ROUTES POUR GÉRER LE SCRAPING
 Route::get('/test-scraper', [ScraperController::class, 'test']);
 
