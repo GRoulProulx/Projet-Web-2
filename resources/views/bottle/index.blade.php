896df26952407edf34dc79c683e964d6cb59cbbd
sylsylviane <syl.sylviane1@gmail.com>
2025-04-21 13:11:59 -0400

    Modifier style CSS pour le catalogue des vins

--- resources/views/bottle/index.blade.php
+++ resources/views/bottle/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <section>
-    <div class="py-md sm:py-md">
+    <div>
         <div class="mx-auto">
             <!-- Grand titre -->
             <header class="max-w-5xl">
@@ -15,12 +15,12 @@
             <div class="mx-auto grid max-w-2xl grid-cols-1 gap-sm gap-y-sm mt-md md:mx-0 md:max-w-none md:grid-cols-2 xl:grid-cols-3 ">
                 @foreach($bottles as $bottle)
                 <a href="{{ route('bottle.show', $bottle->id) }}">
-                    <article class="flex max-w-xl min-h-full flex-col items-start justify-between border border-light-gray/30 rounded-md p-sm hover:border-light-gray/60 transition duration-300 ease-in-out">
-                        <figure class="flex items-center gap-x-4 text-xs">
+                    <article class="flex max-w-full min-h-full flex-col items-start justify-between border border-light-gray/30 rounded-md p-sm hover:border-light-gray/60 transition duration-300 ease-in-out">
+                        <figure class="flex gap-x-sm xs:gap-x-xxs s:gap-x-sm text-xs">
                             <img src="{{ $bottle->image }}" alt="{{ $bottle->name }}" class="object-cover">
                             <figcaption class="flex flex-col gap-xxs flex-wrap">
                                 <header>
-                                    <h2 class="text-lg uppercase">{{ $bottle->name }}</h2>
+                                    <h2 class="xs:text-base sm:text-md md:text-lg uppercase">{{ $bottle->name }}</h2>
                                 </header>
                                 <div class="flex gap-xs flex-wrap">
                                     <p>{{ $bottle->type }}</p>
@@ -38,7 +38,9 @@
         </div>
     </div>
 
-    <a href="{{ route('bottle.create') }}" class="bouton mt-md">Ajouter une bouteille</a>
+    <div class="mt-md">
+        <a href="{{ route('bottle.create') }}" class="bouton">Ajouter une bouteille</a>
+    </div>
 </section>
 
 @endsection
