a26e2d4e9933963bd03218c4a9948f3df6a6175e
GRoulProulx <gproulx85@gmail.com>
2025-04-22 12:34:46 -0400

    Modification de la vue du login

--- resources/views/Auth/create.blade.php
+++ resources/views/Auth/create.blade.php
@@ -3,50 +3,61 @@
 
 <!-- Page de Connexion -->
 
-<div class=" p-md rounded-sm">
-    <div class="">
+<div class="border border-light-gray/30 p-md rounded-sm">
+    <div>
         <h2 class="font-regular font-family-title mx-2">Connectez-vous</h2>
-
-        <form action="{{ route('login') }}" method=" post">
+        <form method="post" action="{{ route('login.store') }}">
             @csrf
-            <div class="p-xxs">
-                <input type="text" placeholder="Nom d'utilisateur"
-                    class="w-full p-xxs my-2 rounded-md focus:outline-none focus:light-gray bg-white" />
-                <input type="email" placeholder="Courriel"
-                    class="w-full p-xxs my-2 rounded-md focus:outline-none focus:light-gray bg-white" />
-                <input type="password" placeholder="Mot de passe"
-                    class="w-full p-xxs my-2 rounded-md focus:outline-none focus:light-gray bg-white" />
+            <div class=" p-xxs">
+                <input type="email" name="email" placeholder="Courriel"
+                    class="border border-light-gray/30 w-full p-xxs my-2 rounded-md focus:outline-none focus:light-gray bg-white" value="{{old('email')}}" required />
+                @if($errors->has('email'))
+                <div>
+                    {{$errors->first('email')}}
+                </div>
+                @endif
+                <input type="password" name="password" placeholder="Mot de passe"
+                    class="border border-light-gray/30 w-full p-xxs my-2 rounded-md focus:outline-none focus:light-gray " required />
+                @if($errors->has('password'))
+                <div>
+                    {{$errors->first('password')}}
+                </div>
+                @endif
             </div>
 
             <div class="flex p-xs">
-                <input type="checkbox" class="mr-3" />
+                <input type="checkbox" name="terms" class="mr-3" />
                 <p class="text-xxs font-light">J'accepte les <span class="font-regular text-">Termes & Politiques de confidentialité</span></p>
+                @if($errors->has('terms'))
+                <div>
+                    {{$errors->first('terms')}}
+                </div>
+                @endif
             </div>
+
+            <div class="relative flex items-center py-5">
+                <div class="flex-grow border-t border-gray-300"></div>
+                <span class="flex-shrink mx-4 text-xxs text-light-gray">Continuer avec</span>
+                <div class="flex-grow border-t border-gray-300"></div>
+            </div>
+
+            <div class="p-xxs flex-col justify-between">
+                <button type="button" class="text-xxs w-full p-xs my-2 border border-light-gray/30 rounded-md focus:outline-none focus:light-gray bg-white cursor-pointer flex items-center justify-center">
+                    <i class="fab fa-facebook mr-2"></i> Facebook
+                </button>
+                <button type="button" class="text-xxs w-full p-xs my-2 border border-light-gray/30 rounded-md focus:outline-none focus:light-gray bg-white cursor-pointer flex items-center justify-center">
+                    <i class="fab fa-google mr-2"></i> Google
+                </button>
+                <button type="button" class="text-xxs w-full p-xs my-2 border border-light-gray/30 rounded-md focus:outline-none focus:light-gray bg-white cursor-pointer flex items-center justify-center">
+                    <i class="fab fa-apple mr-2"></i> Apple
+                </button>
+            </div>
+
+            <button type="submit" class="bouton w-full">Connexion</button>
         </form>
 
-        <div class="">
-            <hr class="">
-            <span class="text-xxs flex justify-center">
-                Continuer avec
-            </span>
-        </div>
-
-        <div class="p-xxs flex-col justify-between">
-            <button class="text-xxs w-full p-xs m-2 border rounded-md focus:outline-none focus:light-gray bg-white cursor-pointer">
-                <i class="fab fa-facebook"></i> Facebook
-            </button>
-            <button class="text-xxs w-full p-xs m-2 border rounded-md focus:outline-none focus:light-gray bg-white cursor-pointer">
-                <i class="fab fa-google"></i> Google
-            </button>
-            <button class="text-xxs w-full p-xs m-2 border rounded-md focus:outline-none focus:light-gray bg-white cursor-pointer">
-                <i class="fab fa-apple"></i> Apple
-            </button>
-        </div>
-
-        <button type="submit" class="bouton w-full">Je m'inscris</button>
-
         <p class="text-xxs flex justify-center p-xxs">
-            Déjà membre? <a href="#" class="px-xxs underline"> Se connecter</a>
+            Pas membre? <a href="{{ route('user.create') }}" class="px-xxs underline"> S'inscrire</a>
         </p>
     </div>
 </div>
