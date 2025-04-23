1a7777b1492f6500e3ea29888e5d0e9359fca632
sylsylviane <syl.sylviane1@gmail.com>
2025-04-23 09:54:58 -0400

    Modifier la table cellar

--- app/Models/Cellar.php
+++ app/Models/Cellar.php
@@ -4,27 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Cellar extends Model
-{
+{ 
     use HasFactory;
 
     protected $fillable = [
-        'purchase_date',
-        'storage_until',
-        'notes',
-        'price',
-        'quantity',
-        'vintage',
-        'user_id'
+        'name', 
+        'user_id',
     ];
 
     // Définit la relation entre le modèle Cellar et le modèle User
     // Un Cellar appartient à un User
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
+    // Définit la relation entre le modèle Cellar et le modèle CellarBottle
+    // Un Cellar peut avoir plusieurs CellarBottle
+    public function cellarBottles(): BelongsToMany
+    {
+        return $this->belongsToMany(CellarBottle::class, 'cellar_bottles_has_cellars');
+    }
 
 }
