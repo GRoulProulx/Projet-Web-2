401864408f7af4cb8b2e5c6c8d1be73bde2b5013
sylsylviane <syl.sylviane1@gmail.com>
2025-04-24 20:05:37 -0400

    Ajouter des labels pour le formulaire pour modifier une bouteille

--- resources/views/bottle/edit.blade.php
+++ resources/views/bottle/edit.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title', trans('Modifier une bouteille'))
+@section('title', 'Modifier une bouteille')
 @section('content')
 <section>
     <header class="mb-md text-center">
@@ -10,64 +10,88 @@
     <form method="POST" class="flex flex-col gap-sm md:max-w-3xl mx-auto">
         @csrf
         @method('PUT')
-        <input type="text" id="name" name="name" value="{{ old('name', $bottle->name) }}" placeholder="Nom" aria-label="Nom" class="border border-light-gray/30 rounded-md p-xs" required>
-        @if($errors->has('name'))
-        <div>
-            {{$errors->first('name')}}
+        <div class="flex flex-col gap-xxs">
+            <label for="name">Nom</label>
+            <input type="text" id="name" name="name" value="{{ old('name', $bottle->name) }}" placeholder="Nom" aria-label="Nom" class="border border-light-gray/30 rounded-md p-xs" required>
+            @if($errors->has('name'))
+            <div>
+                {{$errors->first('name')}}
+            </div>
+            @endif
         </div>
-        @endif
 
-        <input type="text" id="image" name="image" value="{{ old('image', $bottle->image) }}" placeholder="Image" aria-label="Image" class="border border-light-gray/30 rounded-md p-xs" required>
-        @if($errors->has('image'))
-        <div>
-            {{$errors->first('image')}}
+        <div class="flex flex-col gap-xxs">
+            <label for="image">Image</label>
+            <input type="text" id="image" name="image" value="{{ old('image', $bottle->image) }}" placeholder="Image" aria-label="Image" class="border border-light-gray/30 rounded-md p-xs" required>
+            @if($errors->has('image'))
+            <div>
+                {{$errors->first('image')}}
+            </div>
+            @endif
         </div>
-        @endif
 
-        <input type="text" id="price" name="price" value="{{ old('price', $bottle->price) }}" placeholder="Prix" aria-label="Prix" class="border border-light-gray/30 rounded-md p-xs" required>
-        @if($errors->has('price'))
-        <div>
-            {{$errors->first('price')}}
+        <div class="flex flex-col gap-xxs">
+            <label for="price">Prix</label>
+            <input type="text" id="price" name="price" value="{{ old('price', $bottle->price) }}$" placeholder="Prix" aria-label="Prix" class="border border-light-gray/30 rounded-md p-xs" required>
+            @if($errors->has('price'))
+            <div>
+                {{$errors->first('price')}}
+            </div>
+            @endif
         </div>
-        @endif
 
-        <input type="text" id="type" name="type" value="{{ old('type', $bottle->type) }}" placeholder="Type" aria-label="Type" class="border border-light-gray/30 rounded-md p-xs" required>
-        @if($errors->has('type'))
-        <div>
-            {{$errors->first('type')}}
+        <div class="flex flex-col gap-xxs">
+            <label for="type">Type</label>
+            <input type="text" id="type" name="type" value="{{ old('type', $bottle->type) }}" placeholder="Type" aria-label="Type" class="border border-light-gray/30 rounded-md p-xs" required>
+            @if($errors->has('type'))
+            <div>
+                {{$errors->first('type')}}
+            </div>
+            @endif
         </div>
-        @endif
 
-        <input type="text" id="format" name="format" value="{{ old('format', $bottle->format) }}" placeholder="Format" aria-label="Format" class="border border-light-gray/30 rounded-md p-xs" required>
-        @if($errors->has('format'))
-        <div>
-            {{$errors->first('format')}}
+        <div class="flex flex-col gap-xxs">
+            <label for="format">Format</label>
+            <input type="text" id="format" name="format" value="{{ old('format', $bottle->format) }}" placeholder="Format" aria-label="Format" class="border border-light-gray/30 rounded-md p-xs" required>
+            @if($errors->has('format'))
+            <div>
+                {{$errors->first('format')}}
+            </div>
+            @endif
         </div>
-        @endif
 
-        <input type="text" id="country" name="country" value="{{ old('country', $bottle->country) }}" placeholder="Pays" aria-label="Pays" class="border border-light-gray/30 rounded-md p-xs" required>
-        @if($errors->has('country'))
-        <div>
-            {{$errors->first('country')}}
+        <div class="flex flex-col gap-xxs">
+            <label for="country">Pays</label>
+            <input type="text" id="country" name="country" value="{{ old('country', $bottle->country) }}" placeholder="Pays" aria-label="Pays" class="border border-light-gray/30 rounded-md p-xs" required>
+            @if($errors->has('country'))
+            <div>
+                {{$errors->first('country')}}
+            </div>
+            @endif
         </div>
-        @endif
 
-        <input type="text" id="code_saq" name="code_saq" value="{{ old('code_saq', $bottle->code_saq) }}" placeholder="Code SAQ" aria-label="Code SAQ" class="border border-light-gray/30 rounded-md p-xs" required>
-        @if($errors->has('code_saq'))
-        <div>
-            {{$errors->first('code_saq')}}
+        <div class="flex flex-col gap-xxs">
+            <label for="code_saq">Code SAQ</label>
+            <input type="text" id="code_saq" name="code_saq" value="{{ old('code_saq', $bottle->code_saq) }}" placeholder="Code SAQ" aria-label="Code SAQ" class="border border-light-gray/30 rounded-md p-xs" required>
+            @if($errors->has('code_saq'))
+            <div>
+                {{$errors->first('code_saq')}}
+            </div>
+            @endif
         </div>
-        @endif
 
-        <input type="text" id="url" name="url" value="{{ old('url', $bottle->url) }}" placeholder="Lien SAQ" aria-label="Lien SAQ" class="border border-light-gray/30 rounded-md p-xs" required>
-        @if($errors->has('url'))
-        <div>
-            {{$errors->first('url')}}
+        <div class="flex flex-col gap-xxs">
+            <label for="url">Lien SAQ</label>
+            <input type="text" id="url" name="url" value="{{ old('url', $bottle->url) }}" placeholder="Lien SAQ" aria-label="Lien SAQ" class="border border-light-gray/30 rounded-md p-xs" required>
+            @if($errors->has('url'))
+            <div>
+                {{$errors->first('url')}}
+            </div>
+            @endif
         </div>
-        @endif
 
-        <button type="submit" class="bouton mt-0">Sauvegarder</button>
-        <a href="{{ route('bottle.index') }}" class="bouton white mt-0">Annuler</a>
+            <button type="submit" class="bouton mt-0">Sauvegarder</button>
+            <a href="{{ route('bottle.index') }}" class="bouton white mt-0">Annuler</a>
     </form>
 </section>
 @endsection
