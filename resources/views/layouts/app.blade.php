beac684e0b6ca0822654ad42f7eb880411a3a3cb
sylsylviane <syl.sylviane1@gmail.com>
2025-04-28 11:30:31 -0400

    Enlever les classes hidden et ajouter lien tailwind pour tester le déploiement

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,12 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>@yield('title')</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@3.3.2/dist/tailwind.min.css" rel="stylesheet">
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
 </head>
 
 <body class="flex flex-col min-h-[100vh]">
-    <header class="hidden flex justify-between items-center mb-8 text-md m-md">
+    <header class=" flex justify-between items-center mb-8 text-md m-md">
         <i class="fas fa-bars "></i>
         <h1 class="font-title text-center">MAISON DES VINS</h1>
         <i class="fas fa-search "></i>
@@ -20,7 +21,7 @@
         @yield('content')
     </main>
     <footer>
-        <div class="hidden mx-auto text-md p-md">
+        <div class="mx-auto text-md p-md">
             <div class="flex justify-around">
                 <i class="fas fa-home"></i>
                 <i class="fas fa-book-open"></i>
