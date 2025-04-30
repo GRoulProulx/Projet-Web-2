34271877bd90a491b71fe0fcedd564f288e1e7c1
sylsylviane <syl.sylviane1@gmail.com>
2025-04-30 16:28:27 -0400

    Créer la vue pour afficher les détails d'une bouteille d'un cellier

--- resources/views/cellar_bottle/show.blade.php
+++ resources/views/cellar_bottle/show.blade.php
@@ -3,6 +3,62 @@
 @section('content')
 
 <section>
+    <article class="mx-auto relative max-w-3xl border border-light-gray/30 rounded-md p-sm">
+        <!-- <i class="fa-regular fa-heart absolute top-sm left-sm text-lg text-taupe"></i> -->
+        <div class="flex gap-sm flex-wrap">
+            <img src="{{$cellarBottle->bottle->image}}" alt="{{$cellarBottle->bottle->image}}" class="max-w-[111px] max-h-[166px] object-cover mx-auto">
+            <div class="flex flex-col gap-xs">
+                <header>
+                    <h2 class="xs:text-base sm:text-md md:text-lg uppercase">{{$cellarBottle->bottle->name}}</h2>
+                </header>
+                <div class="flex gap-xs flex-wrap ">
+                    <p>{{$cellarBottle->bottle->type}}</p>
+                    <div class="border-2 border-l border-light-gray"></div>
+                    <p>{{$cellarBottle->bottle->format}}</p>
+                    <div class="border-2 border-l border-light-gray"></div>
+                    <p>{{$cellarBottle->bottle->country}}</p>
+                </div>
+                <div class="flex flex-col gap-y-xs">
+                    <p>Date d'achat: {{$cellarBottle->purchase_date}}</p>
+                    <p>Garder jusqu'à: {{$cellarBottle->storage_until}}</p>
+                </div>
+                <div class="flex gap-y-xs">
+                    <!-- <p>Quantité: {{$cellarBottle->quantity}}</p> -->
+                    <form action="" method="post">
+                        @csrf
+                        @method('PUT')
+                        <label for="quantity" class="">Quantité</label>
+                        <input type="number" id="quantity" name="quantity" min="1" max="{{$cellarBottle->quantity}}" value="{{$cellarBottle->quantity}}" class="border border-light-gray rounded-md p-xs w-20">
+                        <input type="submit" value="Boire" class="bouton blue-magenta mt-0">
+                    </form>
+                </div>
+                <div class="flex flex-col gap-y-xs">
+
+                    <p>Prix d'achat: {{$cellarBottle->price}} $</p>
+                    <p>Note (1: mauvais - 5: excellent): {{$cellarBottle->notes}}</p>
+                    <p>Code SAQ: {{$cellarBottle->bottle->code_saq}}</p>
+                    <a href="{{$cellarBottle->bottle->url}}" target="_blank" class="link-underline-hover max-w-fit">Commander à la SAQ <i class="fa-solid fa-arrow-up-right-from-square text-taupe"></i></a>
+                </div>
+
+
+            </div>
+        </div>
+        <div class="mt-md flex gap-md flex-col align-center justify-items-center">
+            <div class="flex gap-sm justify-between flex-wrap ">
+                <a href="{{ route('cellar_bottle.edit', $cellarBottle->id)}}" class="bouton blue-magenta mt-0">Modifier</a>
+                <form action="" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="bouton alert mt-0">Supprimer</button>
+                </form>
+            </div>
+        </div>
+    </article>
+
+    <div class="text-center mt-md">
+
+    </div>
+
 
 </section>
 
