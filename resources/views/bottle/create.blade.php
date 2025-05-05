9f03411641c1a6c02f5f6bb4a327b5d669f2bb6e
sylsylviane <syl.sylviane1@gmail.com>
2025-05-05 13:51:02 -0400

    Ajouter style CSS pour les erreurs de validation

--- resources/views/bottle/create.blade.php
+++ resources/views/bottle/create.blade.php
@@ -10,62 +10,62 @@
         @csrf
         <input type="text" id="name" name="name" value="{{ old('name') }}" placeholder="Nom" aria-label="Nom" class="border border-light-gray/30 rounded-md p-xs" required>
         @if($errors->has('name'))
-        <div>
-            {{$errors->first('name')}}
+        <div class="bg-alert/70 text-white rounded-md p-xs">
+            {{ $errors->first('name') }}
         </div>
         @endif
 
-        <input type="text" id="image" name="image" value="{{ old('image') }}" placeholder="Image" aria-label="Image" class="border border-light-gray/30 rounded-md p-xs" required>
+        <input type="text" id="image" name="image" value="{{asset('images/bouteille-par-defaut.jpg')}}" readonly placeholder="Image" aria-label="Image" class="border border-light-gray/30 rounded-md p-xs" required>
         @if($errors->has('image'))
-        <div>
+        <div class="bg-alert/70 text-white rounded-md p-xs">
             {{$errors->first('image')}}
         </div>
         @endif
 
         <input type="number" id="price" step="0.01" name="price" value="{{ old('price') }}" placeholder="Prix" aria-label="Prix" class="border border-light-gray/30 rounded-md p-xs" required>
         @if($errors->has('price'))
-        <div>
+        <div class="bg-alert/70 text-white rounded-md p-xs">
             {{$errors->first('price')}}
         </div>
         @endif
 
         <input type="text" id="type" name="type" value="{{ old('type') }}" placeholder="Type" aria-label="Type" class="border border-light-gray/30 rounded-md p-xs" required>
         @if($errors->has('type'))
-        <div>
+        <div class="bg-alert/70 text-white rounded-md p-xs">
             {{$errors->first('type')}}
         </div>
         @endif
 
         <input type="text" id="format" name="format" value="{{ old('format') }}" placeholder="Format" aria-label="Format" class="border border-light-gray/30 rounded-md p-xs" required>
         @if($errors->has('format'))
-        <div>
+        <div class="bg-alert/70 text-white rounded-md p-xs">
             {{$errors->first('format')}}
         </div>
         @endif
 
         <input type="text" id="country" name="country" value="{{ old('country') }}" placeholder="Pays" aria-label="Pays" class="border border-light-gray/30 rounded-md p-xs" required>
         @if($errors->has('country'))
-        <div>
+        <div class="bg-alert/70 text-white rounded-md p-xs">
             {{$errors->first('country')}}
         </div>
         @endif
 
         <input type="text" id="code_saq" name="code_saq" value="{{ old('code_saq') }}" placeholder="Code SAQ" aria-label="Code SAQ" class="border border-light-gray/30 rounded-md p-xs" required>
         @if($errors->has('code_saq'))
-        <div>
+        <div class="bg-alert/70 text-white rounded-md p-xs">
             {{$errors->first('code_saq')}}
         </div>
         @endif
 
         <input type="text" id="url" name="url" value="{{ old('url') }}" placeholder="Lien SAQ" aria-label="Lien SAQ" class="border border-light-gray/30 rounded-md p-xs" required>
         @if($errors->has('url'))
-        <div>
+        <div class="bg-alert/70 text-white rounded-md p-xs">
             {{$errors->first('url')}}
         </div>
         @endif
 
         <button type="submit" class="bouton mt-0">Sauvegarder</button>
-        <a href="{{ route('bottle.index') }}" class="bouton white mt-0">Annuler</a>
+        <a href="{{ route('bottle.index') }}" class="bouton white mt-0 text-center">Annuler</a>
     </form>
 </section>
 @endsection
