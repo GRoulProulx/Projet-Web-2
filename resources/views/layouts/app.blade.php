f2cf6e1534ffa6348bae1a4617150851d8dd6783
sylsylviane <syl.sylviane1@gmail.com>
2025-05-06 09:55:02 -0400

    Créer html pour le menu de navigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,11 +11,28 @@
 </head>
 
 <body class="flex flex-col min-h-[100vh]">
-    <header class=" flex justify-between items-center mb-8 text-md m-md">
-        <i class="fas fa-bars "></i>
-        <h1 class="font-title text-center text-lg text-transparent bg-clip-text bg-gradient-to-b from-black via-gray-500 to-gray-100">MAISON DES VINS</h1>
+    <header class="flex items-center justify-between p-md">
+        <div class="off-screen-menu z-10 fixed top-0 left-[-500px] bg-white h-screen w-full max-w-[500px] flex flex-col items-center justify-center text-center text-lg transition-all duration-500 ease-in-out">
+            <ul>
+                <li><a href="#" class="hover:text-taupe">Accueil</a></li>
+                <li><a href="{{route('bottle.index')}}" class="hover:text-taupe">Catalogue des vins</a></li>
+                <li><a href="{{route('login')}}" class="hover:text-taupe">Se connecter</a></li>
+                <li><a href="{{route('user.create')}}" class="hover:text-taupe">S'inscrire</a></li>
+                @if (Auth::check())
+                <li><a href="{{route('cellar.index')}}" class="hover:text-taupe">Mes celliers</a></li>
+                @endif
+            </ul>
+        </div>
 
-        <i class="fas fa-search "></i>
+        <nav class="flex">
+            <div class="ham-menu h-[50px] w-[30px] relative cursor-pointer z-11">
+                <span class="h-[3px] w-full bg-blue-magenta rounded-[25px] absolute left-1/2 top-1/5 -translate-x-1/2 transition duration-300 ease-in-out"></span>
+                <span class="h-[3px] w-full bg-blue-magenta rounded-[25px] absolute left-1/2 top-2/5 -translate-x-1/2 transition duration-300 ease-in-out"></span>
+                <span class="h-[3px] w-full bg-blue-magenta rounded-[25px] absolute left-1/2 top-3/5 -translate-x-1/2 transition duration-300 ease-in-out"></span>
+            </div>
+        </nav>
+        <h1 class="font-title text-center text-lg text-transparent bg-clip-text bg-gradient-to-b from-black via-gray-500 to-gray-100">MAISON DES VINS</h1>
+        <i class="fas fa-search text-lg"></i>
     </header>
     <main class="grow m-md mb-xxl">
         @yield('content')
@@ -23,12 +40,13 @@
     <footer class="fixed bottom-0 w-full left-0 z-50 bg-white shadow-md ">
         <div class="  mx-auto text-md p-md  ">
             <div class="flex justify-around items-center">
-                <a href="{{ route('cellar.index') }}" ><i class="fas fa-home"></i></a>
-                <a href="{{ route('bottle.index') }}" ><i class="fas fa-book-open"></i></a>
+                <a href="{{ route('cellar.index') }}"><i class="fas fa-home"></i></a>
+                <a href="{{ route('bottle.index') }}"><i class="fas fa-book-open"></i></a>
                 <i class="fas fa-user"></i>
                 <i class="fas fa-search"></i>
             </div>
         </div>
     </footer>
 </body>
+
 </html>
