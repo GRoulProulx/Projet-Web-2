0e8273196dd50f6565eb4535ad2b0dd726693df7
GRoulProulx <gproulx85@gmail.com>
2025-05-12 15:55:50 -0400

    Importations des fichiers et l'ajout de l'écouteur d’événement

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
 import "./bootstrap";
 import { formatWineTitle } from "./formatWineTitles";
 import { showModale } from "./modale";
-
+import { filterWineCards } from "./search";
 function init() {
     window.onload = function () {
         const deleteButton = document.querySelector('[data-action="delete"]');
@@ -22,6 +22,11 @@ function init() {
     hamMenu.addEventListener("click", () => {
         hamMenu.classList.toggle("active");
         offScreenMenu.classList.toggle("active");
+    });
+
+    const searchInput = document.querySelector("#search");
+    searchInput.addEventListener("input", (e) => {
+        filterWineCards(e.target.value.toLowerCase());
     });
 }
 
