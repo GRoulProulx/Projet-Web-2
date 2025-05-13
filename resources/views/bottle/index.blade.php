37948922635359b22bc88f9c378a866cb49a90d6
ricoFat <accram2005@hotmail.com>
2025-05-12 22:52:48 -0400

    Ajout et modification de la fonction index pour y incorporer le tri

--- resources/views/bottle/index.blade.php
+++ resources/views/bottle/index.blade.php
@@ -10,7 +10,25 @@
                 <h1 class="font-family-title text-lg">Catalogue des vins</h1>
                 <p>Explorez une vaste sélection de vins directement issus de la SAQ. Recherchez, filtrez selon vos préférences et ajoutez vos découvertes à votre cellier en toute simplicité.</p>
             </header>
-
+            <!--Formulaire de trie-->
+            <form method="GET" action="{{ route('bottle.index') }}" class="mb-md flex  gap-sm items-center mt-lg">
+                <div>
+                    <label for="sort_by" class=""><i class="fa-solid fa-filter"></i> Trier :</label>
+                    <select name="sort_by" id="sort_by" class="border border-light-gray/20 rounded px-2 py-2 mt2 text-center">
+                        <option value="">-- Aucun tri --</option>
+                        <option value="name_asc" {{ request('sort_by') == 'name_asc' ? 'selected' : '' }}>Nom (A-Z)</option>
+                        <option value="name_desc" {{ request('sort_by') == 'name_desc' ? 'selected' : '' }}>Nom (Z-A)</option>
+                        <option value="country_asc" {{ request('sort_by') == 'country_asc' ? 'selected' : '' }}>Pays (A-Z)</option>
+                        <option value="country_desc" {{ request('sort_by') == 'country_desc' ? 'selected' : '' }}>Pays (Z-A)</option>
+                        <option value="price_asc" {{ request('sort_by') == 'price_asc' ? 'selected' : '' }}>Prix (croissant)</option>
+                        <option value="price_desc" {{ request('sort_by') == 'price_desc' ? 'selected' : '' }}>Prix (décroissant)</option>
+                        <option value="type_asc" {{ request('sort_by') == 'type_asc' ? 'selected' : '' }}>Type (A-Z)</option>
+                        <option value="type_desc" {{ request('sort_by') == 'type_desc' ? 'selected' : '' }}>Type (Z-A)</option>
+                    </select>
+                </div>
+                <button type="submit" class="bouton blue-magenta mt-0">Appliquer</button>
+            </form>
+            
             <!-- Grille de produits -->
             <div class="mx-auto grid max-w-2xl grid-cols-1 gap-sm gap-y-sm mt-md md:mx-0 md:max-w-none md:grid-cols-2 xl:grid-cols-3 ">
                 @foreach($bottles as $bottle)
