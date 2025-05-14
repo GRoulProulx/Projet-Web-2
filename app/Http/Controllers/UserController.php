80d7b225a7ca92240b6f9721e95bcaf897928879
sylsylviane <syl.sylviane1@gmail.com>
2025-05-14 12:01:42 -0400

    Créer les méthodes pour gérer les mots de passe oubliés

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Cellar;
-
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Mail;
 class UserController extends Controller
 {
     /**
@@ -85,9 +86,54 @@ class UserController extends Controller
     }
     //TODO: Ajouter des méthodes pour l'oubli de mot de passe, la réinitialisation du mot de passe, etc.
 
+    /**
+     * Méthode pour afficher le formulaire d'oubli de mot de passe
+     * @return \Illuminate\View\View
+     */
     public function forgot() {
         return view('user.forgot');
     }
-    public function passwordEmail() {}
-    public function resetPassword() {}
+
+    /**
+     * Méthode pour envoyer le lien de réinitialisation de mot de passe
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function email(Request $request){
+        //Validation des données du formulaire
+        $request->validate([
+            'email' => 'required|email|exists:users',
+        ]);
+
+        //Récupération de l'utilisateur
+        $user = User::where('email', $request->email)->first();
+        $userId = $user->id;
+        $tempPassword = str::random(45);
+        $user->temp_password = $tempPassword;
+        $user->save();
+
+        //Envoi de l'email
+        $to_name = $user->name;
+        $to_email = $request->email;
+        $body = "<a href='" . route('user.reset', [$userId, $tempPassword]) . "'>Cliquez ici pour recevoir un nouveau mot de passe</a>";
+
+        Mail::send('user.mail', ['name' => $to_name, 'body' => $body], function($message) use ($to_email){
+            $message->to($to_email)->subject('Réinitialisation de mot de passe');
+        });
+        return redirect(route('login'))->with('success', 'Un email de réinitialisation de mot de passe a été envoyé à votre adresse email.');
+    }
+
+    /**
+     * Méthode pour afficher le formulaire de réinitialisation de mot de passe
+     * @param User $user
+     * @param string $token
+     * @return \Illuminate\View\View
+     * @throws \Illuminate\Http\RedirectResponse
+     */
+    public function reset(User $user, $token) {
+        if ($user->temp_password === $token) {
+            return view('user.reset');
+        }
+        return redirect(route('user.forgot'))->withErrors('Les informations fournies ne sont pas valides.');   
+    }
 }
