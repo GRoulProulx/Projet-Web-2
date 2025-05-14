947754fece0852e70b75b9ea606c0b40547abd27
sylsylviane <syl.sylviane1@gmail.com>
2025-05-14 08:25:24 -0400

    Ajouter le cellier dans le localStorage pour pouvoir l'afficher dans le formulaire d'ajout de bouteille

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,10 @@ import { formatWineTitle } from "./formatWineTitles";
 import { showModale } from "./modale";
 
 function init() {
+    const hamMenu = document.querySelector(".ham-menu");
+    const offScreenMenu = document.querySelector(".off-screen-menu");
+
+    // Afficher la modale de confirmation de suppression
     window.onload = function () {
         const deleteButton = document.querySelector('[data-action="delete"]');
         if (deleteButton) {
@@ -10,19 +14,42 @@ function init() {
                 showModale();
             });
         }
+        // Vérifier si on peut récupérer le nom du cellier
+        const myCellar = document.querySelector(".my_cellar");
+        if (myCellar) {
+            const cellarId = myCellar.querySelector("#cellar_id").value;
+            if (cellarId) {
+                localStorage.setItem("cellarId", cellarId);
+            }
+        }
     };
 
+    // Formater les titres de vin
     document.querySelectorAll("h2").forEach((title) => {
         title.textContent = formatWineTitle(title.textContent);
     });
 
-    const hamMenu = document.querySelector(".ham-menu");
-    const offScreenMenu = document.querySelector(".off-screen-menu");
-
+    // Menu hamburger
     hamMenu.addEventListener("click", () => {
         hamMenu.classList.toggle("active");
         offScreenMenu.classList.toggle("active");
     });
+
+    // Sélectionner le cellier dans le formulaire d'ajout de bouteille dans le catalogue des vins si on arrive de la vue cellar.show
+    const formAddBottleInCellar = document.querySelector(".form_add_bottle");
+    const selectCellar = formAddBottleInCellar.querySelector("#select_name_cellar");   
+    // Vérifier si le nom du cellier est déjà dans le localStorage
+    const cellarIdInStorage = localStorage.getItem("cellarId");
+
+    if (cellarIdInStorage) {
+        const option = selectCellar.querySelector(
+            `option[value="${cellarIdInStorage}"]`
+        );
+
+        if (option) {
+            option.selected = true;
+        }
+    }
 }
 
 init(); 
