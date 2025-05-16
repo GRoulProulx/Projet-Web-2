dad4eaf902b7e3af7992656237870bbb655b52bf
GRoulProulx <gproulx85@gmail.com>
2025-05-16 11:51:29 -0400

    Ajout de la table de la liste d'achat

---
+++ database/migrations/2025_05_16_141828_create_shopping_list_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shopping_list', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('bottle_id')->constrained()->onDelete('cascade');
+            $table->integer('quantity');
+            $table->timestamps();
+            
+            // S'assurer qu'un utilisateur ne peut avoir qu'une seule instance d'un produit dans sa liste d'achat
+            $table->unique(['user_id', 'bottle_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shopping_list');
+    }
+};
