72f89e0d2134e64eac37d1aa862e25b59f1d0a7f
sylsylviane <syl.sylviane1@gmail.com>
2025-05-16 14:19:55 -0400

    Tranférer les méthodes pour la gestion du profil dans AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,12 +6,14 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
-
-
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+ 
 class AuthController extends Controller
 {
     /**
-     * Show the form for creating a new resource.
+     * Méthode pour afficher le formulaire de connexion
+     * @return \Illuminate\View\View La vue du formulaire de connexion.
      */
     public function create()
     {
@@ -20,7 +22,9 @@ class AuthController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Méthode pour gérer la soumission du formulaire de connexion
+     * @param Request $request Les données du formulaire de connexion.
+     * @return \Illuminate\Http\RedirectResponse Redirige vers la page du catalogue ou affiche un message d'erreur.
      */
     public function store(Request $request)
     {
@@ -45,31 +49,83 @@ class AuthController extends Controller
     }
 
     /**
-     * Display the specified resource.
+     * Méthode pour afficher le profil de l'utilisateur
+     * @param string $id L'ID de l'utilisateur.
+     * @return \Illuminate\View\View La vue du profil de l'utilisateur.
      */
     public function show(string $id)
     {
-        //
+        if (Auth::user()->id != $id) {
+            abort(403, 'Vous n\'avez pas les droits pour accéder à cette page.');
+        }
+        return view('auth.show', ['user' => $id]);
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * Méthode pour afficher le formulaire d'édition du profil de l'utilisateur
+     * @param string $id L'ID de l'utilisateur.
+     * @return \Illuminate\View\View La vue du formulaire d'édition du profil de l'utilisateur.
      */
     public function edit(string $id)
     {
-        //
+        if (Auth::user()->id != $id) {
+            abort(403, 'Vous n\'avez pas les droits pour accéder à cette page.');
+        }
+        return view('auth.edit', ['user' => $id]);
     }
 
     /**
-     * Update the specified resource in storage.
+     * Méthode pour mettre à jour le profil de l'utilisateur
+     * @param Request $request Les données du formulaire d'édition du profil.
+     * @param string $id L'ID de l'utilisateur.
+     * @return \Illuminate\Http\RedirectResponse Redirige vers la page du profil de l'utilisateur.
      */
     public function update(Request $request, string $id)
     {
-        //
+        //Validation des données du formulaire
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'nullable|min:5|confirmed',
+        ]);
+
+        // si le mot de passe est fourni, on le hache
+        if ($request->filled('password')) {
+            $request->merge(['password' => Hash::make($request->password)]);
+        } else {
+            //on récupère le mot de passe actuel
+            $user = User::find($id);
+            $request->merge(['password' => $user->password]);
+        }
+
+        //Création d'un nouvel utilisateur
+        $user = User::find($id);
+        if (!$user) {
+            return redirect()->route('login')->withErrors('Utilisateur non trouvé.');
+        }
+
+        $user->fill($request->all());
+        $user->save();
+
+        return redirect()->route('auth.show', $id)->with('success', 'Informations mises à jour avec succès.');
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Méthode pour supprimer le profil de l'utilisateur
+     * @param string $id L'ID de l'utilisateur.
+     * @return \Illuminate\Http\RedirectResponse Redirige vers la page de connexion.
+     */
+    public function deleteProfile(string $id){
+        $user = User::find($id);
+        $user->delete();
+        Session::flush();
+        Auth::logout();
+        return redirect(route('login'))->with('success', 'Votre compte a été supprimé avec succès.');
+    }
+
+    /**
+     * Méthode pour se déconnecter de l'application
+     * @return \Illuminate\Http\RedirectResponse Redirige vers la page de connexion.
      */
     public function destroy()
     {
