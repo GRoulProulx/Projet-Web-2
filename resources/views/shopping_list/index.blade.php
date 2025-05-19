bd31eee92dd6add2bdb72b58d95729d8f8a52fca
GRoulProulx <gproulx85@gmail.com>
2025-05-19 14:16:13 -0400

    Ajout d'un prix total des bouteilles dans la liste d'achat

--- resources/views/shopping_list/index.blade.php
+++ resources/views/shopping_list/index.blade.php
@@ -8,7 +8,8 @@
             <!-- Grand titre -->
             <header class="max-w-5xl">
                 <h1 class="font-family-title text-lg">Liste d'achat</h1>
-                <p>Voici votre liste d'achat. C'est ici que vous pourriez organiser vos prochains achats</p>
+                <p>Voici votre liste d'achat. C'est ici que vous pourriez organiser vos prochains achats.</p>
+                <p class="mt-sm font-semibold">Total: {{ $shoppingListItems->sum(function($item) { return $item->bottle->price; }) }} $</p>
             </header>
 
             <!-- Grille des produits -->
