9f95183d71d49591e44cb51accab39025cb3846a
ricoFat <accram2005@hotmail.com>
2025-05-20 08:15:55 -0400

    ajout des icones sur les modales

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,28 +18,25 @@
         <nav class="off-screen-menu z-10 fixed top-0 right-[-10000px] bg-white h-screen w-full flex flex-col text-center text-lg transition-all duration-500 ease-in-out">
             <ul class="mt-[100px] min-w-full">
                 <img src="{{asset('images/logo-maison-des-vins.png')}}" alt="Logo" class="w-[100px] mx-auto mb-sm">
-                <li><a href="{{route('bottle.index')}}" class="text-md p-sm text-blue-magenta hover:text-taupe link-underline-hover">Catalogue des vins</a></li>
+
+                <li><a href="{{route('bottle.index')}}" class="text-md p-sm text-blue-magenta hover:text-taupe link-underline-hover"><i class="fas fa-home mr-2" aria-label="Accueil"></i>Catalogue des vins</a></li>
                 <div class="border-t border-light-gray/50"></div>
-                @auth
-                @if(auth()->user()->role_id == 1)
-                <li><a href="{{route('user.index')}}" class="text-md p-sm text-blue-magenta hover:text-taupe link-underline-hover">Tableau de bord</a></li>
-                <div class="border-t border-light-gray/50"></div>
-                @endif
-                @endauth
                 @if (Auth::check())
-                <li><a href="{{route('logout')}}" class="text-md p-sm text-blue-magenta hover:text-taupe link-underline-hover">Se déconnecter</a></li>
+                <li><a href="{{route('logout')}}" class="text-md p-sm text-blue-magenta hover:text-taupe link-underline-hover"> <i class="fa-solid fa-right-from-bracket mr-2"></i>Se déconnecter</a></li>
                 <div class="border-t border-light-gray/50"></div>
-                <li><a href="{{ route('auth.show', auth()->user()->id) }}" class="text-md p-sm text-blue-magenta hover:text-taupe link-underline-hover">Mon profil</a></li>
+                <li><a href="{{ route('auth.show', auth()->user()->id) }}" class="text-md p-sm text-blue-magenta hover:text-taupe link-underline-hover"> <i class="fas fa-user mr-2
+                    "></i>Mon profil</a></li>
                 <div class="border-t border-light-gray/50"></div>
                 @else
-                <li><a href="{{route('login')}}" class="text-md p-sm text-blue-magenta hover:text-taupe link-underline-hover">Se connecter</a></li>
+                <li><a href="{{route('login')}}" class="text-md p-sm text-blue-magenta hover:text-taupe link-underline-hover"><i class="fa-solid fa-right-to-bracket mr-2"></i> Se connecter</a></li>
                 <div class="border-t border-light-gray/50"></div>
                 <li><a href="{{route('user.create')}}" class="text-md p-sm text-blue-magenta hover:text-taupe link-underline-hover">S'inscrire</a></li>
                 <div class="border-t border-light-gray/50"></div>
                 @endif
                 @if (Auth::check())
-                <li><a href="{{route('cellar.index')}}" class="text-md p-sm text-blue-magenta hover:text-taupe link-underline-hover">Mes celliers</a></li>
+                <li><a href="{{route('cellar.index')}}" class="text-md p-sm text-blue-magenta hover:text-taupe link-underline-hover"> <i class="fas fa-book-open mr-2 "></i>Mes celliers</a></li>
                 <div class="border-t border-light-gray/50"></div>
+
                 @endif
             </ul>
         </nav>
