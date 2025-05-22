2e2fbc957ee276cc766d4bf4bb1af03f372f58c3
sylsylviane <syl.sylviane1@gmail.com>
2025-05-22 11:05:04 -0400

    Nettoyer le code du répertoire Auth

--- resources/views/Auth/create.blade.php
+++ resources/views/Auth/create.blade.php
@@ -1,50 +1,22 @@
 @extends('layouts.app')
 @section('title', 'Connexion')
-
 @section('content')
 
 <!-- Page de Connexion -->
-
-<div class="border border-light-gray/30 p-md rounded-md flex flex-col gap-sm md:max-w-3xl mx-auto ">
+<section class="border border-light-gray/30 p-md rounded-md flex flex-col gap-sm md:max-w-3xl mx-auto ">
     <div>
-        <h1 class="font-family-title mx-2 text-lg">Connectez-vous</h1>
+        <header>
+            <h1 class="font-family-title text-lg">Connectez-vous</h1>
+        </header>
         <form method="post" action="{{ route('login.store') }}">
             @csrf
-            <div class=" p-xxs">
+            <div>
                 <input type="email" name="email" placeholder="Courriel"
-                    class="border border-light-gray/30 w-full p-xxs my-2 rounded-md focus:outline-none focus:light-gray bg-white" value="{{old('email')}}" required />
+                    class="border border-light-gray/30 w-full p-xxs my-2 rounded-md focus:outline-none focus:light-gray bg-white" value="{{old('email')}}" aria-label="Courriel" required />
                 <input type="password" name="password" placeholder="Mot de passe"
-                    class="border border-light-gray/30 w-full p-xxs my-2 rounded-md focus:outline-none focus:light-gray" required />
+                    class="border border-light-gray/30 w-full p-xxs my-2 rounded-md focus:outline-none focus:light-gray" aria-label="Mot de passe" required />
             </div>
-
-            <div class="hidden p-xs">
-                <input type="checkbox" name="terms" class="mr-3" />
-                <p class="text-xxs font-light">J'accepte les <span class="font-regular text-">Termes & Politiques de confidentialité</span></p>
-                @if($errors->has('terms'))
-                <div>
-                    {{$errors->first('terms')}}
-                </div>
-                @endif
-            </div>
-
-            <div class="hidden relative flex items-center py-5">
-                <div class="flex-grow border-t border-gray-300"></div>
-                <span class="flex-shrink mx-4 text-xxs text-light-gray">Continuer avec</span>
-                <div class="flex-grow border-t border-gray-300"></div>
-            </div>
-
-            <div class="hidden p-xxs flex-col justify-between">
-                <button type="button" class="text-xxs w-full p-xs my-2 border border-light-gray/30 rounded-md focus:outline-none focus:light-gray bg-white cursor-pointer flex items-center justify-center">
-                    <i class="fab fa-facebook mr-2"></i> Facebook
-                </button>
-                <button type="button" class="text-xxs w-full p-xs my-2 border border-light-gray/30 rounded-md focus:outline-none focus:light-gray bg-white cursor-pointer flex items-center justify-center">
-                    <i class="fab fa-google mr-2"></i> Google
-                </button>
-                <button type="button" class="text-xxs w-full p-xs my-2 border border-light-gray/30 rounded-md focus:outline-none focus:light-gray bg-white cursor-pointer flex items-center justify-center">
-                    <i class="fab fa-apple mr-2"></i> Apple
-                </button>
-            </div>
-            <a href="{{ route('user.forgot') }}" class="px-xxs underline text-taupe">Mot de passe oublié?</a>
+            <a href="{{ route('user.forgot') }}" class="px-xxs underline text-taupe"><small>Mot de passe oublié?</small></a>
             @guest
             <button type="submit" class="bouton w-full">Connexion</button>
             @endguest
@@ -55,12 +27,9 @@
         </div>
         @endauth
     </div>
-
-
     <p class="text-xxs flex justify-center p-xxs">
         Pas membre? <a href="{{ route('user.create') }}" class="px-xxs underline"> S'inscrire</a>
     </p>
+</section>
 
-</div>
-</div>
 @endsection
